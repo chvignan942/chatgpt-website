c8a76811b321b4f874d195846ae87205f081476b
Vignan <vignanchintakuntlawar@my.unt.edu>
2023-12-03 23:47:56 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function showVideos() {
+    // Dummy function to simulate loading videos
+    const videosContent = document.getElementById('videos-content');
+    videosContent.innerHTML = '<p>Video 1</p><p>Video 2</p><p>Video 3</p>';
+}
+
+function sendMessage() {
+    // Dummy function to simulate sending a message
+    const messageInput = document.getElementById('message');
+    alert('Message sent: ' + messageInput.value);
+}
